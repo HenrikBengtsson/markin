675c99d7b31e6e2225aed355a6de41ee01fe97b2
Henrik Bengtsson <hb@aroma-project.org>
2020-08-28 12:39:01 -0700

    TESTS: Add correctness test for mdi_inject()

--- inst/examples/intro.sh
+++ inst/examples/intro.sh
@@ -1,20 +1,22 @@
 #! /usr/bin/env bash
+#' @usage: mdi build intro.sh
 
+# shellcheck disable=SC2034
 MDI_USER=alice
 MDI_HOSTNAME=dev2
 MDI_PWD='~'
 
-mdi_code_block <<...EOF...
+mdi_code_block <<EOF
 msg="Hello world!"
 date --rfc-3339=seconds
-...EOF...
+EOF
 
 
-mdi_code_block <<...EOF...
+mdi_code_block <<EOF
 msg="Hello world!"
-...EOF...
+EOF
 
 
-mdi_code_block <<...EOF...
+mdi_code_block <<EOF
 echo "\$msg"
-...EOF...
+EOF
---
+++ tests/mdi_inject.R
@@ -0,0 +1,18 @@
+message("*** mdi_inject")
+
+path <- system.file("examples", package = "mdi", mustWork = TRUE)
+
+file <- file.path(path, "intro.md")
+truth <- paste0(file, ".truth")
+lines0 <- readLines(truth, warn = FALSE)
+
+lines <- readLines(file, warn = FALSE)
+
+file <- file.path(path, "intro.md")
+lines <- readLines(file, warn = FALSE)
+cat(lines, sep = "\n")
+
+lines2 <- mdi::mdi_inject(lines, barefile = file.path(path, "intro"))
+cat(lines2, sep = "\n")
+
+stopifnot(all.equal(lines2, lines0))
