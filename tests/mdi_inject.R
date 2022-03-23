c41cf8ee0d32de33bc54654e31e6f0bc8417aad3
Henrik Bengtsson <hb@aroma-project.org>
2022-03-23 08:34:01 -0700

    Renamed mdi to markin

--- tests/mdi_inject.R
+++ tests/mdi_inject.R
@@ -1,6 +1,6 @@
-message("*** mdi_inject")
+message("*** markin_inject")
 
-path <- system.file("examples", package = "mdi", mustWork = TRUE)
+path <- system.file("examples", package = "markin", mustWork = TRUE)
 
 file <- file.path(path, "intro.md")
 truth <- file.path(path, "intro.updated.md")
@@ -10,7 +10,7 @@ file <- file.path(path, "intro.md")
 lines <- readLines(file, warn = FALSE)
 cat(lines, sep = "\n")
 
-lines2 <- mdi::mdi_inject(lines, barefile = file.path(path, "intro"), verbose = TRUE)
+lines2 <- markin::markin_inject(lines, barefile = file.path(path, "intro"), verbose = TRUE)
 cat(lines2, sep = "\n")
 
 stopifnot(all.equal(lines2, lines0))
